be9bb6fd3aafe60006bc553808113cdb88129acc
excitebike <excitebike@6fc31f52-0639-0410-b868-d9777565f53a>
2007-09-01 21:46:21 +0000

    First import

---
+++ Source/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Ringtones
+//
+//  Created by Elliott Harris on 8/22/07.
+//  Copyright __MyCompanyName__ 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
